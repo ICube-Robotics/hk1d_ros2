ea8d6d80c8ea9f541709b6efb3e0c7adf472d151
Thibault Poignonec <thibault.poignonec@gmail.com>
2023-11-08 17:07:01 +0100

    fix + clean-up  description and bringup

--- hk1d_bringup/launch/hk1d.launch.py
+++ hk1d_bringup/launch/hk1d.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
+# Copyright 2023 ICube Laboratory, University of Strasbourg
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,108 +14,157 @@
 
 
 from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-import yaml
-from launch import LaunchContext
+
 
 def generate_launch_description():
-
-    
     # Declare arguments
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
-            'isReal_Slave',
-            default_value='false',
-            description='if true, a real slave robot will be expected, otherwise, a simulated one will be launched',
+            'runtime_config_package',
+            default_value='hk1d_bringup',
+            description='Config package with the various runtime config files (ros2 controllers, gazebo, etc.).',
         )
     )
-
     declared_arguments.append(
         DeclareLaunchArgument(
-            'isReal_Master',
+            'description_package',
+            default_value='hk1d_description',
+            description='Description package with robot URDF/xacro files. Usually the argument \
+                         is not set, it enables use of a custom description.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'description_file',
+            default_value='hk1d.config.xacro',
+            description='URDF/XACRO description file with the robot.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'prefix',
+            default_value='""',
+            description='Prefix of the joint/link names, useful for multi-robot setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='/',
+            description='Namespace of launched nodes, useful for multi-robot setup. \
+                         If changed than also the namespace in the controllers \
+                         configuration needs to be updated. Expected format "<ns>/".',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim',
             default_value='false',
-            description='if true, a real Master robot will be expected, otherwise, a simulated one will be launched',
+            description='Start robot in Gazebo simulation.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_fake_hardware',
+            default_value='true',
+            description='Start robot with fake hardware mirroring command to its states.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'command_interface',
+            default_value='force',
+            description='Robot command interface [position|velocity|effort].',
         )
     )
 
-    # declared_arguments.append(
-    #     DeclareLaunchArgument(
-    #         'control_freq',
-    #         default_value='800',
-    #         description='control frequency, will be passed to hardware controller in (hk1d.r2c_hardware.xacro)',
-    #     )
-    # )
-
     # Initialize Arguments
-    isReal_Slave = LaunchConfiguration('isReal_Slave')
-    isReal_Master = LaunchConfiguration('isReal_Master')
-
-    # control_freq = LaunchConfiguration('control_freq')
-
-    lc = LaunchContext()
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare("hk1d_bringup"),
-            "config",
-            "hk1d_controllers.yaml",
-        ]
-    )
-
-    with open(robot_controllers.perform(lc)) as info:
-      info_dict = yaml.load(info,yaml.FullLoader)
-      print("update rate is:", info_dict['controller_manager']['ros__parameters']['update_rate'])
-    set_membership_params = info_dict['ar_mpc_controller']['ros__parameters']
-    control_freq = info_dict['controller_manager']['ros__parameters']['update_rate']
-    Ts = 1/control_freq
+    runtime_config_package = LaunchConfiguration('runtime_config_package')
+    description_package = LaunchConfiguration('description_package')
+    description_file = LaunchConfiguration('description_file')
+    prefix = LaunchConfiguration('prefix')
+    namespace = LaunchConfiguration('namespace')
+    use_sim = LaunchConfiguration('use_sim')
+    use_fake_hardware = LaunchConfiguration('use_fake_hardware')
+    command_interface = LaunchConfiguration('command_interface')
 
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
             PathJoinSubstitution(
-                [
-                    FindPackageShare("hk1d_description"),
-                    "urdf",
-                    "hk1d.config.xacro",
-                ]
+                [FindPackageShare(description_package), 'config', description_file]
             ),
             ' ',
-            'isReal_Slave:=',
-            isReal_Slave,
+            'prefix:=', prefix,
             ' ',
-            'isReal_Master:=',
-            isReal_Master,
+            'namespace:=', namespace,
             ' ',
-            'control_freq:=',
-            str(control_freq),
+            'use_sim:=', use_sim,
             ' ',
-            'Ts:=',
-            str(Ts),
+            'use_fake_hardware:=', use_fake_hardware,
+            ' ',
+            'command_interface:=', command_interface,
+            ' ',
+            'description_package:=', description_package,
+            ' ',
+            'runtime_config_package:=', runtime_config_package,
         ]
     )
 
-    robot_description = {"robot_description": robot_description_content}
-    
+    robot_description = {'robot_description': robot_description_content}
+
+    # Get SRDF via xacro
+    def get_robot_description_semantic_content():
+        return Command([
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "srdf", "hk1d.srdf.xacro"]
+            ),
+            " ",
+            "name:=", "iiwa",
+            " ",
+            "prefix:=", prefix,
+            " ",
+            'description_package:=', description_package,
+            ' ',
+            'runtime_config_package:=', runtime_config_package,
+        ])
+
+    # robot_description_semantic = {
+    #     'robot_description_semantic': get_robot_description_semantic_content()
+    # }
+
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("hk1d_description"), "rviz", "hk1d.rviz"]
     )
 
     # logger = launch.substitutions.LaunchConfiguration("log_level");
 
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(runtime_config_package),
+            'config',
+            'hk1d_controllers.yaml',
+        ]
+    )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         # parameters=[]
-        parameters=[robot_description, robot_controllers,{'isReal_Slave': isReal_Slave},{'isReal_Master': isReal_Master},{'control_freq':str(control_freq)},{'Ts':Ts}],
+        parameters=[robot_description, robot_controllers],
         output="both",
         # arguments=['--ros-args', '--log-level', "debug"]
     )
+
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -123,6 +172,7 @@ def generate_launch_description():
         parameters=[robot_description],
         # arguments=['--ros-args', '--log-level', "debug"]
     )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -137,89 +187,11 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    armpc_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["ar_mpc_controller", "-c", "/controller_manager"],
-    )  
-
-    simple_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["teleop_sim2sim_simple_controller", "-c", "/controller_manager"],
-    )  
-
-    plotJuggler_layout_file = [
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("hk1d_bringup"),
-                    "config",
-                    "PlotJuggler_layout.xml",
-                ]
-            ),
-        ]
-    plotJuggler_Node = Node(
-        package="plotjuggler",
-        executable="plotjuggler",
-        arguments=["-l",plotJuggler_layout_file],
-    )  
-
-    set_membership_params = PathJoinSubstitution(
-        [
-            FindPackageShare("set_membership"),
-            "config",
-            "parameters.yaml",
-        ]
-    )
-
-    node_set_membership = Node(
-        package="set_membership",
-        executable="set_membership_node",
-        # namespace="",
-        # prefix=["gdbserver localhost:3000"], #for debugging
-        output="screen",
-        parameters=[set_membership_params,{'Ts':Ts}]
-        # parameters=[set_membership_params,{'Ts':Ts}],
-    )
-    print("Launch file Ts = ", Ts)
-
-    teleop_real2sim_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["teleop_real2sim_controller", "-c", "/controller_manager"],
-    )  
-
-    teleop_real2real_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["teleop_real2real_controller", "-c", "/controller_manager"],
-    ) 
-
-    teleop_sim2sim_simple_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["teleop_sim2sim_simple_controller", "-c", "/controller_manager"],
-    ) 
-
-    if (not isReal_Master) and (not isReal_Slave):
-       controller_Node = teleop_sim2sim_simple_controller_spawner
-    elif(isReal_Master) and (not isReal_Slave):
-       controller_Node = teleop_real2sim_controller_spawner
-    else:
-    #    controller_Node = teleop_real2real_controller_spawner
-       controller_Node = armpc_controller_spawner
-        
-
     nodes = [
         control_node,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
-        # node_set_membership,
-        ## simple_controller_spawner,
-        ## plotJuggler_Node,
-        ## rviz_node,
-        controller_Node,
-       
+        rviz_node,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
